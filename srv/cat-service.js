6b552b6ab2334669e50db5ac336d237439735e20
francocapra <franco.capra@outlook.com>
2019-11-21 11:10:42 +0000

    Build 1.0

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -1,21 +1,48 @@
-module.exports = (srv) => {
+// module.exports = (srv) => {
 
- // Reply mock data for Books...
- srv.on ('READ', 'Books', ()=>{
-  console.log('Reading a Books');
-  return [
-    { ID:201, title:'Wuthering Heights', author_ID:101, stock:12 },
-    { ID:251, title:'The Raven', author_ID:150, stock:333 },
-    { ID:252, title:'Eleonora', author_ID:150, stock:555 },
-    { ID:271, title:'Catweazle', author_ID:170, stock:222 },
-  ]
- })
+//  // Reply mock data for Books...
+//  srv.on ('READ', 'Books', ()=>{
+//   console.log('Reading a Books');
+//   return [
+//     { ID:201, title:'Wuthering Heights', author_ID:101, stock:12 },
+//     { ID:251, title:'The Raven', author_ID:150, stock:333 },
+//     { ID:252, title:'Eleonora', author_ID:150, stock:555 },
+//     { ID:271, title:'Catweazle', author_ID:170, stock:222 },
+//   ]
+//  })
 
- // Reply mock data for Authors...
- srv.on ('READ', 'Authors', ()=>[
-   { ID:101, name:'Emily Brontë' },
-   { ID:150, name:'Edgar Allen Poe' },
-   { ID:170, name:'Richard Carpenter' },
- ])
+//  // Reply mock data for Authors...
+//  srv.on ('READ', 'Authors', ()=>[
+//    { ID:101, name:'Emily Brontë' },
+//    { ID:150, name:'Edgar Allen Poe' },
+//    { ID:170, name:'Richard Carpenter' },
+//  ])
+
+// }
+// @ts-check
+  module.exports = (srv) => {
+
+  // @ts-ignore
+  const {Books} = cds.entities ('my.bookshop')
+
+  // Reduce stock of ordered books
+  srv.before ('CREATE', 'Orders', async (req) => {
+    const order = req.data
+    if (!order.amount || order.amount <= 0)  return req.error (400, 'Order at least 1 book')
+    // @ts-ignore
+    const tx = cds.transaction(req)
+    const affectedRows = await tx.run (
+      // @ts-ignore
+      UPDATE (Books)
+        .set   ({ stock: {'-=': order.amount}})
+        .where ({ stock: {'>=': order.amount},/*and*/ ID: order.book_ID})
+    )
+    if (affectedRows === 0)  req.error (409, "Sold out, sorry")
+  })
+
+  // Add some discount for overstocked books
+  srv.after ('READ', 'Books', each => {
+    if (each.stock > 111)  each.title += ' -- 11% discount!'
+  })
 
 }
